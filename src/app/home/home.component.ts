348ca12f60ef3b45e56d13e1c8d3d32aa0e77794
Kevin Paul Merwa <115960408+kevinmerwa@users.noreply.github.com>
2025-06-30 00:57:37 +0800

    replaced old codes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  contactInfo = [
+    { type: "LOCATION", info: "Culaba, Biliran, PH" },
+    { type: "EMAIL", info: "kevinpaulmerwa@gmail.com" },
+    { type: "PHONE", info: "+63-927-272-5447" },
+    { type: "GITHUB", info: "@kmerwa" },
+    { type: "LINKEDIN", info: "in/kmerwa" },
+  ]
+
+  activeContacts = false;
+
+  toggleContacts() {
+    this.activeContacts = !this.activeContacts;
+  }
 
 }
